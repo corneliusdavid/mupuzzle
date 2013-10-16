da08e6561c0174b17129666fb1da906f80de8118
David Cornelius <dev@corneliusconcepts.com>
2013-10-15 23:33:19 -0700

    started WinRT version

---
+++ oxy_WinRT/MU_Puzzle_WinRT/MU_Puzzle_WinRT.oxygene
@@ -0,0 +1,146 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<Project ToolsVersion="4.0" DefaultTargets="Build" xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
+  <PropertyGroup>
+    <Configuration Condition=" '$(Configuration)' == '' ">Debug</Configuration>
+    <Platform Condition=" '$(Platform)' == '' ">AnyCPU</Platform>
+    <ProjectGuid>{37d1276e-9da3-4d81-ab80-c6e68c482efd}</ProjectGuid>
+    <OutputType>AppContainerExe</OutputType>
+    <AppDesignerFolder>Properties</AppDesignerFolder>
+    <RootNamespace>MU_Puzzle_WinRT</RootNamespace>
+    <AssemblyName>MU_Puzzle_WinRT</AssemblyName>
+    <DefaultLanguage>en-US</DefaultLanguage>
+    <ProjectTypeGuids>{BC8A1FFA-BEE3-4634-8014-F334798102B3};{656346D9-4656-40DA-A068-22D5425D4639}</ProjectTypeGuids>
+    <PackageCertificateKeyFile>MU_Puzzle_WinRT_TemporaryKey.pfx</PackageCertificateKeyFile>
+    <Name>MU_Puzzle_WinRT</Name>
+  </PropertyGroup>
+  <PropertyGroup Condition=" '$(Configuration)|$(Platform)' == 'Debug|AnyCPU' ">
+    <CpuType>AnyCPU</CpuType>
+    <GeneratePDB>True</GeneratePDB>
+    <GenerateMDB>False</GenerateMDB>
+    <Optimize>false</Optimize>
+    <OutputPath>bin\Debug\</OutputPath>
+    <DefineConstants>DEBUG;TRACE;NETFX_CORE</DefineConstants>
+  </PropertyGroup>
+  <PropertyGroup Condition=" '$(Configuration)|$(Platform)' == 'Release|AnyCPU' ">
+    <CpuType>AnyCPU</CpuType>
+    <GeneratePDB>True</GeneratePDB>
+    <GenerateMDB>False</GenerateMDB>
+    <Optimize>true</Optimize>
+    <OutputPath>bin\Release\</OutputPath>
+    <DefineConstants>TRACE;NETFX_CORE</DefineConstants>
+  </PropertyGroup>
+  <PropertyGroup Condition="'$(Configuration)|$(Platform)' == 'Debug|ARM'">
+    <DebugSymbols>true</DebugSymbols>
+    <GeneratePDB>True</GeneratePDB>
+    <GenerateMDB>False</GenerateMDB>
+    <OutputPath>bin\ARM\Debug\</OutputPath>
+    <DefineConstants>DEBUG;TRACE;NETFX_CORE</DefineConstants>
+    <CpuType>ARM</CpuType>
+    <Prefer32Bit>true</Prefer32Bit>
+  </PropertyGroup>
+  <PropertyGroup Condition="'$(Configuration)|$(Platform)' == 'Release|ARM'">
+    <OutputPath>bin\ARM\Release\</OutputPath>
+    <GeneratePDB>True</GeneratePDB>
+    <GenerateMDB>False</GenerateMDB>
+    <DefineConstants>TRACE;NETFX_CORE</DefineConstants>
+    <Optimize>true</Optimize>
+    <CpuType>ARM</CpuType>
+    <Prefer32Bit>true</Prefer32Bit>
+  </PropertyGroup>
+  <PropertyGroup Condition="'$(Configuration)|$(Platform)' == 'Debug|x64'">
+    <DebugSymbols>true</DebugSymbols>
+    <GeneratePDB>True</GeneratePDB>
+    <GenerateMDB>False</GenerateMDB>
+    <OutputPath>bin\x64\Debug\</OutputPath>
+    <DefineConstants>DEBUG;TRACE;NETFX_CORE</DefineConstants>
+    <CpuType>x64</CpuType>
+    <Prefer32Bit>true</Prefer32Bit>
+  </PropertyGroup>
+  <PropertyGroup Condition="'$(Configuration)|$(Platform)' == 'Release|x64'">
+    <OutputPath>bin\x64\Release\</OutputPath>
+    <GeneratePDB>True</GeneratePDB>
+    <GenerateMDB>False</GenerateMDB>
+    <DefineConstants>TRACE;NETFX_CORE</DefineConstants>
+    <Optimize>true</Optimize>
+    <CpuType>x64</CpuType>
+    <Prefer32Bit>true</Prefer32Bit>
+  </PropertyGroup>
+  <PropertyGroup Condition="'$(Configuration)|$(Platform)' == 'Debug|x86'">
+    <DebugSymbols>true</DebugSymbols>
+    <GeneratePDB>True</GeneratePDB>
+    <GenerateMDB>False</GenerateMDB>
+    <OutputPath>bin\x86\Debug\</OutputPath>
+    <DefineConstants>DEBUG;TRACE;NETFX_CORE</DefineConstants>
+    <CpuType>x86</CpuType>
+    <Prefer32Bit>true</Prefer32Bit>
+  </PropertyGroup>
+  <PropertyGroup Condition="'$(Configuration)|$(Platform)' == 'Release|x86'">
+    <OutputPath>bin\x86\Release\</OutputPath>
+    <GeneratePDB>True</GeneratePDB>
+    <GenerateMDB>False</GenerateMDB>
+    <DefineConstants>TRACE;NETFX_CORE</DefineConstants>
+    <Optimize>true</Optimize>
+    <CpuType>x86</CpuType>
+    <Prefer32Bit>true</Prefer32Bit>
+  </PropertyGroup>
+  <ItemGroup>
+    <!-- A reference to the entire .Net Framework and Windows SDK are automatically included -->
+    <Folder Include="Assets\" />
+    <Folder Include="Common\" />
+    <Folder Include="Properties\" />
+  </ItemGroup>
+  <ItemGroup>
+    <Compile Include="App.xaml.pas">
+      <DependentUpon>App.xaml</DependentUpon>
+    </Compile>
+    <Compile Include="MainPage.xaml.pas">
+      <DependentUpon>MainPage.xaml</DependentUpon>
+    </Compile>
+    <Compile Include="Properties\AssemblyInfo.pas" />
+  </ItemGroup>
+  <ItemGroup>
+    <AppxManifest Include="Package.appxmanifest">
+      <SubType>Designer</SubType>
+    </AppxManifest>
+    <None Include="MU_Puzzle_WinRT_TemporaryKey.pfx" />
+  </ItemGroup>
+  <ItemGroup>
+    <Content Include="Assets\Logo.png" />
+    <Content Include="Assets\WideLogo.png" />
+    <Content Include="Assets\SmallLogo.png" />
+    <Content Include="Assets\SplashScreen.png" />
+    <Content Include="Assets\StoreLogo.png" />
+  </ItemGroup>
+  <ItemGroup>
+    <ApplicationDefinition Include="App.xaml">
+      <Generator>MSBuild:Compile</Generator>
+      <SubType>Designer</SubType>
+    </ApplicationDefinition>
+    <Page Include="Common\StandardStyles.xaml">
+      <Generator>MSBuild:Compile</Generator>
+      <SubType>Designer</SubType>
+    </Page>
+    <Page Include="MainPage.xaml">
+      <Generator>MSBuild:Compile</Generator>
+      <SubType>Designer</SubType>
+    </Page>
+  </ItemGroup>
+  <PropertyGroup Condition=" '$(VisualStudioVersion)' == '' or '$(VisualStudioVersion)' &lt; '11.0' ">
+    <VisualStudioVersion>11.0</VisualStudioVersion>
+  </PropertyGroup>
+  <!-- To modify your build process, add your task inside one of the targets below and uncomment it.
+       Other similar extension points exist, see Microsoft.Common.targets.
+  <Target Name="BeforeBuild">
+  </Target>
+  <Target Name="AfterBuild">
+  </Target>
+  -->
+  <ItemGroup>
+    <Reference Include="mscorlib" />
+  </ItemGroup>
+  <Import Project="$(MSBuildExtensionsPath)\$(MSBuildToolsVersion)\Microsoft.Common.props" Condition="Exists('$(MSBuildExtensionsPath)\$(MSBuildToolsVersion)\Microsoft.Common.props')" />
+  <Import Project="$(MSBuildExtensionsPath)\RemObjects Software\Oxygene\RemObjects.Oxygene.Echoes.WinRT.targets" />
+  <PropertyGroup>
+    <PreBuildEvent />
+  </PropertyGroup>
+</Project>
---
+++ oxy_WinRT/MU_Puzzle_WinRT/MainPage.xaml.pas
@@ -0,0 +1,333 @@
+﻿namespace MU_Puzzle_WinRT;
+
+interface
+
+uses
+  System,
+  System.Collections.Generic,
+  System.IO,
+  System.Linq,
+  Windows.Foundation,
+  Windows.Foundation.Collections,
+  Windows.UI.Xaml,
+  Windows.UI.Xaml.Controls,
+  Windows.UI.Xaml.Controls.Primitives,
+  Windows.UI.Xaml.Data,
+  Windows.UI.Xaml.Input,
+  Windows.UI.Xaml.Media,
+  Windows.UI.Xaml.Navigation;
+
+type
+  TRuleMode = (eNoRule, eRule3, eRule4);
+  MainPage = partial class(Page)
+  private
+    WorkStr: String;
+    RuleMode: TRuleMode;
+    SelectorPos: Integer;
+    method UpdateMuStr;
+    method AddU;
+    method DuplicateStr;
+    method StartReplace3IWithU;
+    method Replace3IWithU(const StartPos: Integer);
+    method StartDeleteUU;
+    method DeleteUU(const StartPos: Integer);
+    method CancelRuleMode;
+    method SelectOK;
+    method LaunchRule(const RuleNum: Integer);
+    method ActivateSelBtns(const TurnOn: Windows.UI.Xaml.Visibility);
+    method ActivateRuleBtns(const TurnOn: Windows.UI.Xaml.Visibility);
+    method SetNewWorkingString;
+    method CurrRuleModeStr: String;
+    method NextSelector; 
+    method PrevSelector;
+    method btnRule1_Click(sender: Object; e: Windows.UI.Xaml.RoutedEventArgs);
+    method btnRule2_Click(sender: Object; e: Windows.UI.Xaml.RoutedEventArgs);
+    method btnRule3_Click(sender: Object; e: Windows.UI.Xaml.RoutedEventArgs);
+    method btnRule4_Click(sender: Object; e: Windows.UI.Xaml.RoutedEventArgs);
+      method btnCancel_Click(sender: Object; e: Windows.UI.Xaml.RoutedEventArgs);
+      method btnOK_Click(sender: Object; e: Windows.UI.Xaml.RoutedEventArgs);
+      method btnPrev_Click(sender: Object; e: Windows.UI.Xaml.RoutedEventArgs);
+      method btnNext_Click(sender: Object; e: Windows.UI.Xaml.RoutedEventArgs);
+
+  public
+    constructor ;
+
+  protected
+    method OnNavigatedTo(e: NavigationEventArgs); override;
+  end;
+
+implementation
+
+constructor MainPage;
+begin
+  self.InitializeComponent();
+
+  RuleMode := TRuleMode.eNoRule;
+  WorkStr := 'I';
+  UpdateMuStr;
+end;
+
+method MainPage.OnNavigatedTo(e: NavigationEventArgs);
+begin
+end;
+
+method MainPage.UpdateMuStr;
+begin
+  if RuleMode = TRuleMode.eNoRule then
+    lblMuStr.Text := 'M' + WorkStr
+  else if (RuleMode = TRuleMode.eRule3) or (RuleMode = TRuleMode.eRule4) then
+    if SelectorPos = -1 then
+      lblMuStr.Text := 'M' + WorkStr
+    else begin
+      var ws1 := WorkStr.Substring(0, SelectorPos);
+      var ws2 := WorkStr.Substring(SelectorPos);
+      lblMuStr.Text := 'M' + ws1 + '>' + ws2;
+    end;      
+
+  { add strings to ListBox ignoring duplicates }
+{
+      if (RuleMode = TRuleMode.eNoRule) and ((lbMuStrs.Items.Count = 0) or
+            (String.Compare(lblMuStr.Text.ToString, lbMuStrs.Items. [lbMuStrs.Items.Count-1].ToString) <> 0)) then
+    lbMuStrs.Items.Add(lblMuStr.Text.ToString);
+}
+end;
+
+method MainPage.btnRule1_Click(sender: Object; e: Windows.UI.Xaml.RoutedEventArgs);
+begin
+  LaunchRule(1);
+end;
+
+method MainPage.btnRule2_Click(sender: Object; e: Windows.UI.Xaml.RoutedEventArgs);
+begin
+  LaunchRule(2);
+end;
+
+method MainPage.btnRule3_Click(sender: Object; e: Windows.UI.Xaml.RoutedEventArgs);
+begin
+  LaunchRule(3);
+end;
+
+method MainPage.btnRule4_Click(sender: Object; e: Windows.UI.Xaml.RoutedEventArgs);
+begin
+  LaunchRule(4);
+end;
+
+method MainPage.LaunchRule(RuleNum: Integer);
+begin
+  case RuleNum of
+    1: AddU;
+    2: DuplicateStr;
+    3: StartReplace3IWithU;
+    4: StartDeleteUU;
+  end;
+end;
+
+method MainPage.AddU;
+begin
+  { if the last character is "I", add a "U" }
+  if WorkStr.EndsWith('I') then
+    WorkStr := WorkStr + 'U';
+  UpdateMuStr;
+end;
+
+method MainPage.DuplicateStr;
+begin
+  { add the whole string to the end of itself }
+  WorkStr := WorkStr + WorkStr;
+  UpdateMuStr;
+end;
+
+method MainPage.ActivateSelBtns(TurnOn: Windows.UI.Xaml.Visibility);
+begin
+  SelectGrid.Visibility := TurnOn;
+
+  lblSelect.Visibility := TurnOn;
+  if (TurnOn = Windows.UI.Xaml.Visibility.Visible) then
+      lblSelect.Text := if RuleMode = TRuleMode.eRule3 then
+          "Select: " + "III"  else "Select: " + "UU"
+end;
+
+method MainPage.ActivateRuleBtns(TurnOn: Windows.UI.Xaml.Visibility);
+begin
+  btnRule1.Visibility := TurnOn;
+  btnRule2.Visibility := TurnOn;
+  lblRule1.Visibility := TurnOn;
+  lblRule2.Visibility := TurnOn;
+
+  if RuleMode = TRuleMode.eRule3 then begin
+    btnRule3.IsEnabled := TurnOn = Visibility.Visible;
+    lblRule4.Visibility := TurnOn;
+    btnRule4.Visibility := TurnOn;
+  end else if RuleMode = TRuleMode.eRule4 then begin
+    btnRule3.Visibility := TurnOn;
+    lblRule3.Visibility := TurnOn;
+    btnRule4.IsEnabled := TurnOn = Visibility.Visible;
+  end else begin
+    lblRule3.Visibility := TurnOn;
+    btnRule3.Visibility := TurnOn;
+    btnRule3.IsEnabled := TurnOn = Visibility.Visible;
+    lblRule4.Visibility := TurnOn;
+    btnRule4.Visibility := TurnOn;
+    btnRule4.IsEnabled := TurnOn = Visibility.Visible;
+  end;
+end;
+
+method MainPage.SetNewWorkingString;
+begin
+
+end;
+
+method MainPage.NextSelector;
+begin
+  var NewSelectorPos := WorkStr.IndexOf(CurrRuleModeStr, SelectorPos + 1);
+
+  if NewSelectorPos >= 0 then
+    SelectorPos := NewSelectorPos;
+end;
+
+method MainPage.PrevSelector;
+begin
+  var NewSelectorPos := WorkStr.IndexOf(CurrRuleModeStr, SelectorPos + 1);
+
+  while NewSelectorPos > -1 do
+    if WorkStr.Substring(NewSelectorPos).StartsWith(CurrRuleModeStr) then begin
+      SelectorPos := NewSelectorPos;
+      Break;
+    end else
+      Dec(NewSelectorPos);
+end;
+
+method MainPage.StartReplace3IWithU;
+// replace an occurance of III with U
+var
+  i: Integer;
+  s: String;
+begin
+  // first, count how many occurances of III exist
+
+  var count := 0;
+  for i := 0 to WorkStr.Length - 3 do begin
+    s := WorkStr.Substring(i);
+    if s.StartsWith('III') then
+      inc(count);
+  end;
+
+  { if none, put up a message }
+  if count = 0 then begin
+    var msg := Windows.UI.Popups.MessageDialog.Create('There is no occurance of "III" in the current string.');
+    msg.ShowAsync;
+  end else begin
+    { if one, replace it automatically }
+    if count = 1 then
+      Replace3IWithU(WorkStr.IndexOf('III'))
+    else begin
+      { if more than one, let user select }
+      RuleMode := TRuleMode.eRule3;
+
+      ActivateSelBtns(Windows.UI.Xaml.Visibility.Visible);
+      ActivateRuleBtns(Windows.UI.Xaml.Visibility.Collapsed);
+      SelectorPos := -1;
+      NextSelector;
+    end;
+
+    UpdateMuStr;
+  end;
+end;
+
+method MainPage.Replace3IWithU(StartPos: Integer);
+begin
+  var ws1 := WorkStr.Substring(0, StartPos);
+  var ws2 := WorkStr.Substring(StartPos + 3);
+  WorkStr := ws1 + "U" + ws2;
+end;
+
+method MainPage.StartDeleteUU;
+// delete an occurance of UU
+var
+  i: Integer;
+  s: String;
+begin
+  // first, count how many occurances of UU exist
+  var count := 0;
+  for i := 0 to WorkStr.Length - 2 do begin
+    s := WorkStr.Substring(i);
+    if s.StartsWith('UU') then
+      inc(count);
+  end;
+
+  // none?  just show a message 
+  if count = 0 then begin
+    var msg := Windows.UI.Popups.MessageDialog.Create('There is no occurance of ''UU'' in the current string.');
+    msg.ShowAsync;
+  end else begin
+    // just 1? then just replace it
+    if count = 1 then
+      DeleteUU(WorkStr.IndexOf('UU'))
+    else begin
+      // it looks like we'll have to let the user select which occurance
+      RuleMode := TRuleMode.eRule4 ;
+      ActivateSelBtns(Visibility.Visible);
+      ActivateRuleBtns(Visibility.Collapsed);
+      SelectorPos := -1;
+      NextSelector;
+    end;
+    UpdateMuStr;
+  end
+end;
+
+method MainPage.DeleteUU(StartPos: Integer);
+begin
+  var ws1 := WorkStr.Substring(0, StartPos);
+  var ws2 := WorkStr.Substring(StartPos + 2);
+  WorkStr := ws1 + ws2;
+end;
+
+method MainPage.CancelRuleMode;
+begin
+  RuleMode := TRuleMode.eNoRule;
+  ActivateSelBtns(Visibility.Collapsed);
+  ActivateRuleBtns(Visibility.Visible);
+  UpdateMuStr;
+end;
+
+method MainPage.SelectOK;
+begin
+  if RuleMode = TRuleMode.eRule3 then
+    Replace3IWithU(SelectorPos)
+  else
+    DeleteUU(SelectorPos);
+
+  CancelRuleMode;
+end;
+
+method MainPage.CurrRuleModeStr: String;
+begin
+  if RuleMode in (TRuleMode.eRule3, TRuleMode.eRule4) then
+    Result := if RuleMode = TRuleMode.eRule3 then 'III' else 'UU'
+  else
+    Result := '';
+end;
+
+method MainPage.btnCancel_Click(sender: Object; e: Windows.UI.Xaml.RoutedEventArgs);
+begin
+  CancelRuleMode;
+end;
+
+method MainPage.btnOK_Click(sender: Object; e: Windows.UI.Xaml.RoutedEventArgs);
+begin
+  SelectOK;
+end;
+
+method MainPage.btnPrev_Click(sender: Object; e: Windows.UI.Xaml.RoutedEventArgs);
+begin
+  PrevSelector;
+  UpdateMuStr;
+end;
+
+method MainPage.btnNext_Click(sender: Object; e: Windows.UI.Xaml.RoutedEventArgs);
+begin
+  NextSelector;
+  UpdateMuStr;
+end;
+
+end.
