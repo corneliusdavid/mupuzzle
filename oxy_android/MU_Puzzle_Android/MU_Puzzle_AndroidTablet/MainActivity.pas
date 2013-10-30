321661565e70a7aa355aadc4b616b30ad506c791
Ezra <dev@corneliusconcepts.com>
2013-10-29 18:49:09 -0700

    Android version started
    layout under way, no real functionality yet

---
+++ oxy_android/MU_Puzzle_Android/MU_Puzzle_AndroidTablet/MainActivity.pas
@@ -0,0 +1,43 @@
+﻿namespace mu_puzzle_androidtablet;
+
+interface
+
+uses
+  java.util,
+  android.app,
+  android.content,
+  android.os,
+  android.util,
+  android.view,
+  android.widget;
+
+type
+  MainActivity = public class(Activity)
+  private
+    Count: Integer := 0;
+  public
+    method onCreate(savedInstanceState: Bundle); override;
+    method ButtonOnClick(v: View);
+  end;
+
+implementation
+
+method MainActivity.onCreate(savedInstanceState: Bundle);
+begin
+  inherited;
+  // Set our view from the "main" layout resource
+  ContentView := R.layout.main;
+
+  // Get our button from the layout resource,
+  // and attach an event to it
+  var myButton: Button := Button(findViewById(R.id.MyButton));
+  myButton.OnClickListener := new interface View.OnClickListener(onClick := @ButtonOnClick);
+end;
+
+method MainActivity.ButtonOnClick(v: View);
+begin
+  inc(Count);
+  (v as Button).Text := WideString.format(String[R.string.my_button_text_2], Count);
+end;
+
+end.
